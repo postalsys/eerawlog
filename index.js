482a9d5d83665af096286ff29aa9b348c648e740
Andris Reinman <andris@reinman.eu>
2021-09-28 16:19:17 +0300

    v1.2.0

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
 
-const fastJsonParse = require('fast-json-parse');
 const pump = require('pump');
 const split = require('split2');
 const { Writable } = require('stream');
 const clc = require('cli-color');
+
+function Parse(data) {
+    if (!(this instanceof Parse)) {
+        return new Parse(data);
+    }
+    this.err = null;
+    this.value = null;
+    try {
+        this.value = JSON.parse(data);
+    } catch (err) {
+        this.err = err;
+        this.input = data;
+    }
+}
 
 class Logger extends Writable {
     constructor(opts) {
@@ -18,52 +31,65 @@ class Logger extends Writable {
     }
 
     _write(chunk, encoding, callback) {
-        let time;
+        try {
+            let time;
 
-        if (chunk && chunk.value) {
-            time = new Date(chunk.value.time).toISOString().substr(0, 19).replace(/T/, ' ');
-        }
-
-        if (chunk && chunk.value && chunk.value.src === 'connection' && chunk.value.host && chunk.value.port) {
-            if (!this.prevConn || this.prevConn.cid !== chunk.value.cid) {
-                this.prevConn = chunk.value;
-                // show connection header
-                console.log(`${clc.bold(`${chunk.value.cid}${chunk.value.account ? ` [${chunk.value.account}]` : ''}`)}`);
+            if (chunk && chunk.err && chunk.input) {
+                console.log(clc.xterm(11)(chunk.input));
             }
 
-            console.log(
-                `${clc.xterm(8)(
-                    `[${time}] Connection established to ${chunk.value.host}:${chunk.value.port} (${chunk.value.secure ? chunk.value.version : 'no tls'})`
-                )}`
-            );
-        }
-
-        if (chunk && chunk.value && chunk.value.src && chunk.value.data) {
-            let prefix = `[${time}] ${chunk.value.src.toUpperCase().trim()}: `;
-            let pad = val => {
-                val = val
-                    .replace(/\r/g, '')
-                    .replace(/\n$/, '')
-                    .replace(/\n/g, `\n${' '.repeat(prefix.length)}`);
-
-                let col = chunk.value.src === 's' ? 39 : 119;
-
-                return clc.xterm(col)(val);
-            };
-            if (!this.prevConn || this.prevConn.cid !== chunk.value.cid) {
-                this.prevConn = chunk.value;
-                // show connection header
-                console.log(`${clc.bold(`${chunk.value.cid}${chunk.value.account ? ` [${chunk.value.account}]` : ''}`)}`);
+            if (chunk && chunk.value) {
+                time = new Date(chunk.value.time || Date.now()).toISOString().substr(0, 19).replace(/T/, ' ');
             }
 
-            console.log(`${clc.xterm(8)(prefix)}${pad(Buffer.from(chunk.value.data, 'base64').toString())}`);
-        }
+            if (chunk && chunk.value && chunk.value.src === 'connection' && chunk.value.host && chunk.value.port) {
+                if (!this.prevConn || this.prevConn.cid !== chunk.value.cid) {
+                    this.prevConn = chunk.value;
+                    // show connection header
+                    console.log(`${clc.bold(`${chunk.value.cid}${chunk.value.account ? ` [${chunk.value.account}]` : ''}`)}`);
+                }
 
+                console.log(
+                    `${clc.xterm(8)(
+                        `[${time}] Connection established to ${chunk.value.host}:${chunk.value.port} (${chunk.value.secure ? chunk.value.version : 'no tls'})`
+                    )}`
+                );
+            }
+
+            if (chunk && chunk.value && chunk.value.src && chunk.value.data) {
+                let prefix = `[${time}] ${chunk.value.src.toUpperCase().trim()}: `;
+                let pad = val => {
+                    val = val
+                        .replace(/\r/g, '')
+                        .replace(/\n$/, '')
+                        .replace(/\n/g, `\n${' '.repeat(prefix.length)}`);
+
+                    let col = chunk.value.src === 's' ? 39 : 119;
+
+                    return clc.xterm(col)(val);
+                };
+                if (!this.prevConn || this.prevConn.cid !== chunk.value.cid) {
+                    this.prevConn = chunk.value;
+                    // show connection header
+                    console.log(`${clc.bold(`${chunk.value.cid}${chunk.value.account ? ` [${chunk.value.account}]` : ''}`)}`);
+                }
+
+                console.log(`${clc.xterm(8)(prefix)}${pad(Buffer.from(chunk.value.data, 'base64').toString())}`);
+            }
+        } catch (err) {
+            console.error(err);
+        } finally {
+            callback();
+        }
+    }
+
+    _final(callback) {
+        console.log(clc.xterm(8)(`input stream ended`));
         callback();
     }
 }
 
-pump(process.stdin, split(fastJsonParse), new Logger({}));
+pump(process.stdin, split(Parse), new Logger({}));
 process.on('SIGINT', function () {
     process.exit(0);
 });
